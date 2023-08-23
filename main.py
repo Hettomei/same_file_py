3a9d98a45995d2f7d45b2712177b78f96e91136d
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-08-23 11:33:29 +0200

    Improve

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import sqlite3
+import argparse
+import glob
 import hashlib
 import os
+from pprint import pprint
+import sqlite3
 import sys
 
-import glob
-from pprint import pprint
+
+def parse_args(sys_args):
+    parser = argparse.ArgumentParser(
+        prog="Same file", description="Find same file based on sha256"
+    )
+    parser.add_argument(
+        "--feed-with",
+        help="Root path for computing all sha256 digest",
+        dest="root_path",
+    )
+
+    parser.add_argument(
+        "--show-same-files",
+        help="Explain which file are duplicated",
+        dest="same_files",
+        action="store_true",
+    )
+    return parser.parse_args(sys_args)
 
 
 def is_db_present(cur):
@@ -32,44 +51,64 @@ CREATE TABLE files(
         cur.execute("CREATE INDEX idx_files_sha256 ON files (sha256)")
 
 
-# Sauvegarder tous les hash en memoire pour un acces plus rapide
-# un hash de 64 bytes, x 800 k fichiers = 50 MO de mémoire dispo
-# 64*800000/(1024*1024)
-# 48.828 MO
-def cache():
-    pass
-
-
-def main(arg_path):
-    print(arg_path)
+def main(args):
     with sqlite3.connect(
         "same_files.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
     ) as con:
         cur = con.cursor()
         initialize_db(cur)
-        # fill_database(con, cur, arg_path)
+        if args.root_path:
+            fill_database(con, cur, args)
 
-        # res = cur.execute("SELECT * FROM files ORDER BY size DESC LIMIT 100")
-        # pprint(res.fetchall())
+        if args.same_files:
+            show_same_files(cur)
 
-        # print("-----------------")
 
-        # res = cur.execute("SELECT * FROM files ORDER BY size ASC LIMIT 100")
-        # pprint(res.fetchall())
+def show_same_files(cur):
+    # res = cur.execute("SELECT * FROM files ORDER BY size DESC LIMIT 100")
+    # pprint(res.fetchall())
 
-        # afiche que les doublon
-        res = cur.execute("""
+    # print("-----------------")
+
+    # res = cur.execute("SELECT * FROM files ORDER BY size ASC LIMIT 100")
+    # pprint(res.fetchall())
+
+    # afiche que les doublon
+    res = cur.execute(
+        """
 SELECT sha256, count(sha256) as count_sha256
 FROM files
 GROUP BY sha256
 HAVING count_sha256 > 1
 ORDER BY count_sha256 DESC
-""")
-        pprint(res.fetchall())
+LIMIT 3
+"""
+    )
+    # We extracted before the for ... so we free the cursor to do other SELECT
+    all_duplicated_digest = [row[0] for row in res]
+    pprint(all_duplicated_digest)
+    for digest in all_duplicated_digest:
+        start_explain(cur, digest)
 
 
-def fill_database(con, cur, arg_path):
-    for path in glob.iglob(os.path.join(arg_path, "**", "*"), recursive=True):
+def start_explain(cur, digest):
+    res = cur.execute(
+        """
+SELECT *
+FROM files
+WHERE sha256 = ?
+ORDER BY path ASC
+""",
+        (digest,),
+    )
+
+    for row in res:
+        print(row)
+    print("----")
+
+
+def fill_database(con, cur, args):
+    for path in glob.iglob(os.path.join(args.root_path, "**", "*"), recursive=True):
         if os.path.isfile(path):
             print(path)
             size_bytes = os.stat(path).st_size
@@ -79,15 +118,15 @@ def fill_database(con, cur, arg_path):
                 print(hexdigest)
                 try:
                     cur.execute(
-                    "insert into files(id, path, name, size, sha256) values (?, ?, ?, ?, ?)",
-                    (None, path, os.path.basename(path), size_bytes, hexdigest),
-                )
+                        "insert into files(id, path, name, size, sha256) values (?, ?, ?, ?, ?)",
+                        (None, path, os.path.basename(path), size_bytes, hexdigest),
+                    )
                     con.commit()
                 except sqlite3.IntegrityError:
                     print("already in dtb")
                 print("--")
-            # print(res.fetchone())
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    args = parse_args(sys.argv[1::1])
+    main(args)
